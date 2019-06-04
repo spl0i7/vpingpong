355ef65385ad1c7b7e979336cac6c7ee0ef6150d
Ketan Singh <ketansingh.sh@gmail.com>
2019-06-04 20:42:52 +0530

    add test for receiver fn for player

--- player_test.go
+++ player_test.go
@@ -17,5 +17,36 @@ func TestNewPlayer(t *testing.T) {
 	if p2.Name != "B" || p2.Mode != MODE_OFFENSIVE || len(p2.DefenseArray) != 3 {
 		t.Fatal("unexpected initialization")
 	}
+}
 
+func TestPlayer_ReverseRole(t *testing.T) {
+	p1 := NewPlayer("A", MODE_DEFENSIVE, newDefenceArray(0))
+	if p1 == nil {
+		t.Fatal("could not create player")
+	}
+
+	old := p1.Mode
+	p1.ReverseRole()
+	if p1.Mode == old {
+		t.Fatal("could not reverse role")
+	}
+
+	p1.ReverseRole()
+
+	if p1.Mode != old {
+		t.Fatal("could not reverse role")
+	}
+}
+
+func TestPlayer_GenerateOffensiveNumber(t *testing.T) {
+	p1 := NewPlayer("A", MODE_OFFENSIVE, newDefenceArray(0))
+	if p1 == nil {
+		t.Fatal("could not create player")
+	}
+
+	for i := 0; i < 1000; i ++ {
+		if p1.GenerateOffensiveNumber() > 10 {
+			t.Fatal("unexpectedly generated number greater 10")
+		}
+	}
 }
