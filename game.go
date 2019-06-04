bc6bf9c73065edef6fc2b37b3606a0c8f16e0338
Ketan Singh <ketansingh.sh@gmail.com>
2019-06-04 22:10:10 +0530

    add test for playing game and minor refactoring in game playing

--- game.go
+++ game.go
@@ -17,35 +17,39 @@ func NewGame(p1, p2 *Player)  *Game {
 			Score: 0,
 		},
 		Second: &ScoreBoard{
-			Player: p1,
+			Player: p2,
 			Score: 0,
 		},
 	}
 }
 
-func (g *Game) Play() {
+func (g *Game) Play() *Player {
 
 	// we already know p1 is offensive and p2 is defensive
 
-	for g.First.Score == 5 || g.Second.Score == 5 {
+	for g.First.Score < 5 || g.Second.Score < 5 {
 
 		var offensive, defensive *ScoreBoard
+
 		if g.First.Player.Mode == MODE_OFFENSIVE {
 			offensive = g.First
 			defensive = g.Second
 		} else {
-			offensive = g.First
-			defensive = g.Second
+			offensive = g.Second
+			defensive = g.First
 		}
 
 		offensiveChoice := offensive.Player.GenerateOffensiveNumber()
-
 		if defensive.Player.IsDefensiveHavingNumber(offensiveChoice) {
 			defensive.Score += 1
 		} else {
 			offensive.Score += 1
 		}
-
 	}
+
+	if g.First.Score == 5 {
+		return g.First.Player
+	}
+	return g.Second.Player
 
 }
--- game_test.go
+++ game_test.go
@@ -6,14 +6,25 @@ func TestGame_Play(t *testing.T) {
 
 
 
+	for i := 0; i < 1000; i++ {
+		p1 := NewPlayer("A", newDefenceArray(4))
+		p2 := NewPlayer("B", newDefenceArray(8))
+		p1.SetDefensiveMode()
+		p2.SetOffensiveMode()
+		g := NewGame(p1, p2)
+		winner := g.Play()
+		if winner == nil {
+			t.Fatal("play should not return null")
+		}
+	}
+
+
 }
 
 func TestNewGame(t *testing.T) {
 
-	p1 := NewPlayer("A", newDefenceArray(3))
-	p2 := NewPlayer("B", newDefenceArray(4))
-	p1.SetDefensiveMode()
-	p2.SetDefensiveMode()
+	p1 := NewPlayer("A", newDefenceArray(7))
+	p2 := NewPlayer("B", newDefenceArray(7))
 
 	g := NewGame(p1, p2)
 
@@ -24,6 +35,5 @@ func TestNewGame(t *testing.T) {
 	if g.First.Score != 0 || g.Second.Score != 0 {
 		t.Fatal("could not initialize game")
 	}
-	
 
 }
